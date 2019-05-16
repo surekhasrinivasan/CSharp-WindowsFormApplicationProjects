035ca8f65f41cb864c8a979d0638cdb5faed3028
Surekhasrinivasan <surekhasrao@gmail.com>
2019-05-15 21:53:55 -0400

    Created groupbox which contains labels and textboxes

--- SimpleLoginForm/SimpleLoginForm/Form1.cs
+++ SimpleLoginForm/SimpleLoginForm/Form1.cs
@@ -16,5 +16,15 @@ namespace SimpleLoginForm
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
